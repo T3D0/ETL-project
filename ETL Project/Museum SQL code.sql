4d90f00deea68ced1bec67086caf1f8e4618a543
T3D0 <42944820+T3D0@users.noreply.github.com>
2018-12-11 20:39:44 -0600

    Update Museum SQL code.sql

--- ETL Project/Museum SQL code.sql
+++ ETL Project/Museum SQL code.sql
@@ -13,7 +13,7 @@ Museum_Name VARCHAR(255) PRIMARY KEY,
 Rating FLOAT
 );
 
-SELECT md.Museum_Name, md.Museum_ID, md.State, mr.Museum_Name, mr.Rating
+SELECT md.Museum_Name, md.Museum_ID, md.State, mr.Rating
 FROM Museum_Directory md
 JOIN Museum_Reviews mr
 ON md.Museum_Name = mr.Museum_Name
