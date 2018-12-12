b37d43311fa640b27b9e9122e599fed9787a9195
T3D0 <42944820+T3D0@users.noreply.github.com>
2018-12-11 20:37:59 -0600

    Add files via upload

---
+++ ETL Project/Museum SQL code.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE Museums_db;
+
+USE Museums_db;
+
+CREATE TABLE Museum_Directory (
+Museum_Name VARCHAR(255) PRIMARY KEY,
+Museum_ID VARCHAR(100),
+State TEXT
+);
+
+CREATE TABLE Museum_Reviews (
+Museum_Name VARCHAR(255) PRIMARY KEY,
+Rating FLOAT
+);
+
+SELECT md.Museum_Name, md.Museum_ID, md.State, mr.Museum_Name, mr.Rating
+FROM Museum_Directory md
+JOIN Museum_Reviews mr
+ON md.Museum_Name = mr.Museum_Name
---
+++ ETL Project/config.py
@@ -0,0 +1,1 @@
+mypass = "Ch!ck3n26"
